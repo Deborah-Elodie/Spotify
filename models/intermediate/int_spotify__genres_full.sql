1554b4791137f47cdd7b53e0000d21ff15289da8
Alexandre Benguigui <contact.alexandrebenguigui@gmail.com>
2026-03-11 14:09:59 +0000

    add genre according to artists already in the table

---
+++ models/intermediate/int_spotify__genres_full.sql
@@ -0,0 +1,42 @@
+-- aliementation des genres nuls a partir de l'artiste de la même table
+WITH liste_artiste_genre AS (-- on crée une table simple avec les artiste + genre principal (il peut y en avoir plusieurs on prends le max)
+    SELECT DISTINCT
+    artist
+    ,MAX(cat_genre) as cat_genre
+  FROM {{ref('int_spotify__genres_join')}}
+  WHERE cat_genre is NOT NULL
+  GROUP BY artist
+)
+
+SELECT 
+    a.track_id
+    ,a.artist
+    ,a.title
+    ,a.album
+    ,a.release_date
+    ,a.streams
+    ,a.ranking
+    ,a.date_date
+    ,a.url_link
+    ,a.country
+    ,a.region
+    ,a.chart
+    ,a.trend
+    ,a.popularity
+    ,a.duration_ms
+    ,a.is_explicit
+    ,a.af_danceability
+    ,a.af_energy
+    ,a.af_speechiness
+    ,a.af_acousticness
+    ,a.af_instrumentalness
+    ,a.af_valence
+    ,a.af_tempo
+    ,CASE 
+    WHEN a.cat_genre IS NOT NULL THEN a.cat_genre
+    WHEN a.cat_genre IS NULL AND b.cat_genre IS NOT NULL THEN b.cat_genre
+    ELSE NULL
+    END AS cat_genre
+    FROM {{ref('int_spotify__genres_join')}} as a
+    LEFT JOIN liste_artiste_genre as b
+    USING (artist)
