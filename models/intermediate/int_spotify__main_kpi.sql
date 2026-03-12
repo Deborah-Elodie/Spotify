d5d9cba9dba4af1d761c6899f86e53747b42e480
aline lains <aline.lains@gmail.com>
2026-03-12 10:23:28 +0000

    ordre des colonnes + format date + format intitulés colonnes

--- models/intermediate/int_spotify__main_kpi.sql
+++ models/intermediate/int_spotify__main_kpi.sql
@@ -2,24 +2,24 @@ with temp as (
     select 
     track_id
     , duration_ms
-    ,(Popularity * 0.6) + (af_danceability * 0.4) as Potentiel_Viral_calcul
-    ,(af_energy * 0.6) + (af_valence *0.4) as Energy_Vibe_calcul
+    ,(Popularity * 0.6) + (af_danceability * 0.4) as potentiel_viral_calcul
+    ,(af_energy * 0.6) + (af_valence *0.4) as energy_vibe_calcul
 from {{ ref('int_spotify__main_join') }}
 )
 select DISTINCT
  track_id
 ,case 
-when Potentiel_Viral_calcul >= 0.75 then 'Viral hits' -- tres populaire tres dansant fort potentiel de diffusion
-when Potentiel_Viral_calcul < 0.4 then 'Niche tracks' -- faible pouplarite et potentiel dansant audiance plus specifique
+when potentiel_viral_calcul >= 0.75 then 'Viral hits' -- tres populaire tres dansant fort potentiel de diffusion
+when potentiel_viral_calcul < 0.4 then 'Niche tracks' -- faible pouplarite et potentiel dansant audiance plus specifique
 else 'Popular tracks' -- performance correcte playlist generaliste
-end as Potentiel_Viral
+end as potentiel_viral
 ,case
-when Energy_Vibe_calcul >= 0.75 then 'Happy & Energetic'
-when Energy_Vibe_calcul < 0.4 then 'Melancholy & Chill'
+when energy_vibe_calcul >= 0.75 then 'Happy & energetic'
+when energy_vibe_calcul < 0.4 then 'Melancholy & chill'
 else 'Balanced' 
-end as Energy_Vibe
+end as energy_vibe
 ,case 
-when duration_ms < 180000 then 'Radio Friendly'
+when duration_ms < 180000 then 'Radio friendly'
 else 'Long play'
 end as duration_segment
 from temp
